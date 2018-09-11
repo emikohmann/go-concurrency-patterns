c5923a7119bc907fe8f4cc4ee3d4b68eeda76011
ekohmann <emiliano.kohmann@mercadolibre.com>
2018-09-11 01:58:35 -0300

    Adding go routines examples

---
+++ routines/main_1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+func main() {
+
+	ProcessUsers()
+}
+
+func ProcessUsers() {
+
+	i := 0
+
+	for true {
+
+		ProcessUser(i)
+
+		i++
+	}
+}
+
+func ProcessUser(userID int) {
+
+    fmt.Println("I'm processing user", userID)
+
+    time.Sleep(time.Duration(200 + rand.Intn(300)) * time.Millisecond)
+}
---
+++ routines/main_3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"    
+)
+
+func main() {
+
+	go ProcessUsers()
+
+	time.Sleep(3 * time.Second)
+}
+
+func ProcessUsers() {
+
+	i := 0
+
+	for true {
+
+		ProcessUser(i)
+
+		i++
+	}
+}
+
+func ProcessUser(userID int) {
+
+    fmt.Println("I'm processing user", userID)
+
+    time.Sleep(time.Duration(200 + rand.Intn(300)) * time.Millisecond)
+}
